5cf1795dfebe671acc8163f2762d73c8bc2c8fb2
Siarhei <palavetssiarhei@gmail.com>
2022-03-05 11:18:01 +0300

    scriptjs mod

--- js/script.js
+++ js/script.js
@@ -1,12 +1,1 @@
 "use strict";
-
-let i=0;
-let arr = ['Nikita','Masha','Natasha','Sergey','Vika'];
-
-    let NikitaTime = prompt ('Никита');
-    let MashaTime = prompt ('Маша');
-    let NatashaTime = prompt ('Наташа');
-    let SergeyTime = prompt ('Сергей');
-    let VikaTime = prompt ('Вика');
-
-for (i=0, i<5, i++ );
