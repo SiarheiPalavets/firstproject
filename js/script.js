40c3355571636f02b6c02ff268addafd7f3c3f02
Siarhei <palavetssiarhei@gmail.com>
2022-03-05 12:42:33 +0300

    movierate

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const numberoffilms = prompt('how many films have you watched', '');
+
+const personalMovieDB = {
+    count: numberoffilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+
+
+};
+
+const lastfilm = prompt('last film u watched', ''),
+    rate = prompt('rate this film', ''),
+    lastfilm1 = prompt('last film u watched', ''),
+    rate1 = prompt('rate this film', '');
+
+personalMovieDB.movies[lastfilm] = rate;
+personalMovieDB.movies[lastfilm1] = rate1;
+
+console.log(personalMovieDB.movies);
