2b8c7a4d64b938fbc7b15d926be8b808324bd7ac
Siarhei <palavetssiarhei@gmail.com>
2022-03-05 15:11:05 +0300

    2nd movierate

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 const numberoffilms = prompt('how many films have you watched', '');
 
@@ -8,16 +8,30 @@ const personalMovieDB = {
     actors: {},
     genres: [],
     privat: false,
-
-
 };
 
-const lastfilm = prompt('last film u watched', ''),
-    rate = prompt('rate this film', ''),
-    lastfilm1 = prompt('last film u watched', ''),
-    rate1 = prompt('rate this film', '');
+for (let i = 0; i < 2; i++) {
+    const lastfilm = prompt('last film u watched', ''),
+        rate = prompt('rate this film', '');
 
-personalMovieDB.movies[lastfilm] = rate;
-personalMovieDB.movies[lastfilm1] = rate1;
+    if (lastfilm != null && rate != null && lastfilm != '' && 
+    rate != '' && lastfilm.length < 50) {
+        personalMovieDB.movies[lastfilm] = rate;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
-console.log(personalMovieDB.movies);
+if (personalMovieDB.count < 10) {
+    console.log('not enough');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    console.log('regular');
+} else if (personalMovieDB.count > 30) {
+    console.log('a lot');
+} else {
+    console.log('error');
+}
+
+console.log(personalMovieDB);
